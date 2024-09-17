46ccf9d7b449b49a9b42fa41af455c79b662f7e6
Munkhdorj-M <64317250+munkhdorj-m@users.noreply.github.com>
2024-09-17 12:19:37 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,21 +1,9 @@
-import numpy as np
-
-def hello_world() -> str:
-    """
-    Implement a function that returns Hello World!
-    """
-    raise NotImplementedError()
+def perform_operations(a, b):
+    # Write you code here
 
 
-def add_two(number: int) -> int:
-    """
-    Adds two to `number`
-    """
-    raise NotImplementedError()
+def check_even_odd(n):
+    # Write you code here
 
 
-def zero_array(N: int) -> np.ndarray:
-    """
-    Returns a double type ndarray of length N filled with zeros 
-    """
-    raise NotImplementedError()
+def multiplication_table(n):
