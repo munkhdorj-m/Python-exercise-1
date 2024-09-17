009b82ecf663a79b0278010be5c170b0a95f40e6
Munkhdorj-M <64317250+munkhdorj-m@users.noreply.github.com>
2024-09-17 14:01:37 +0800

    Update test_assignment.py

--- test_assignment.py
+++ test_assignment.py
@@ -2,14 +2,14 @@ import pytest
 import numpy as np
 from assignment import perform_operations, check_even_odd, compare_numbers
 
-def test_perform_operations():
+def test1:
     # Test for different pairs of numbers
     assert perform_operations(10, 2) == (12, 8, 20, 5)
     assert perform_operations(5, 5) == (10, 0, 25, 1)
     assert perform_operations(7, 3) == (10, 4, 21, 7/3)
 
 @pytest.mark.parametrize("input, expected", [(2, "Even"), (3, "Odd"), (0, "Even"), (-5, "Odd")])
-def test_check_even_odd(input, expected):
+def test2(input, expected):
     # Test for even and odd numbers
     assert check_even_odd(input) == expected
 
@@ -18,6 +18,6 @@ def test_check_even_odd(input, expected):
     (2, 7, "7 is greater than 2"),
     (4, 4, "Both numbers are equal")
 ])
-def test_compare_numbers(num1, num2, expected):
+def test3(num1, num2, expected):
     # Test for comparing two numbers
     assert compare_numbers(num1, num2) == expected
