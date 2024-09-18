0136204c309c61fa426dea048e2e1ad93bc7d8ff
Munkhdorj-M <64317250+munkhdorj-m@users.noreply.github.com>
2024-09-18 10:55:07 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,13 +1,15 @@
+# You can remove 'pass' if you written code in the function 
+
 def perform_operations(a, b):
     # Write your code here
-    raise NotImplementedError()
+    pass
 
 
 def check_even_odd(n):
     # Write your code here
-    raise NotImplementedError()
+    pass
 
 
 def compare_numbers(num1, num2):
     # Write your code here
-    raise NotImplementedError()
+    pass
